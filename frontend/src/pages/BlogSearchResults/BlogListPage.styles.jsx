ead7d7c920221bd90c56186446d12849419fa6e9
Mi Sun Choi <misunchoi@Mis-MacBook-Air.attlocal.net>
2020-06-18 22:38:46 -0700

    make search bar and filter fill mobile width

--- frontend/src/pages/BlogSearchResults/BlogListPage.styles.jsx
+++ frontend/src/pages/BlogSearchResults/BlogListPage.styles.jsx
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 import mixins from '../../styles/mixins';
+import media from '../../styles/media';
 
 export const FlexWrapper = styled.div`
   ${mixins.flexBetween}
+  flex-wrap: wrap;
 `;
 
 export const SearchFieldWrapper = styled.div`
   width: 25%;
+  ${media.phone`width: 100%; padding-bottom: 15px;`};
 `;
 
 export const FilterWrapper = styled.div`
-  display: flex;
+  ${media.phone`width: 100%;`};
 `;
 
 export const PaginationWrapper = styled.div`
