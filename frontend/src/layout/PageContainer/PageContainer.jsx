434f317276e0b88f1dc29ba56d20280df3ed4d84
Michelle (Misun) Choi <choi.msc033@gmail.com>
2020-06-19 00:04:49 -0700

    fix(footer): Make the footer stick to the bottom even if there's no content (#227)
    Co-authored-by: Mi Sun Choi <misunchoi@Mis-MacBook-Air.attlocal.net>

--- frontend/src/layout/PageContainer/PageContainer.jsx
+++ frontend/src/layout/PageContainer/PageContainer.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import mixins from 'styles/mixins';
 
 const PageContainer = styled.div`
+  flex: 1 0 auto;
   padding-top: 64px;
   padding-bottom: 64px;
   ${mixins.responsivePadding}
