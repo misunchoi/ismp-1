1c335610ac4e6db4e670a5d92204adaa035d3b0b
Douglas Chan <dtchan@eng.ucsd.edu>
2020-06-20 11:10:51 -0700

    Move environment variable configuration to docker-compose.yml for easy configuration. (#245)

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -11,15 +11,72 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-if os.getenv("ENV_TYPE") == "PROD":
-    from .prod_settings import *  # pylint: disable=unused-wildcard-import, wildcard-import
-    print("initializing with prod settings")
-else:
-    from .dev_settings import *  # pylint: disable=unused-wildcard-import, wildcard-import
-    print("initializing with dev settings")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv("DEBUG").lower() == "true"
+if DEBUG:
+    print("Starting with debug mode on!")
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_DATABASE"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': int(os.getenv("DB_PORT")),
+    }
+}
+
+ENV_ALLOWED_HOST = os.getenv("ALLOWED_HOST")
+if ENV_ALLOWED_HOST:
+    ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
+else:
+    ALLOWED_HOSTS = []
+
+USE_S3 = os.getenv('USE_S3').lower() == 'true'
+
+# define this outside of the if block even if not using s3 to get
+# rid of an annoying warning from boto3.
+AWS_DEFAULT_ACL = "public-read"
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'api.upload.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'api.upload.storage_backends.PublicMediaStorage'
+    # s3 private media settings
+    PRIVATE_MEDIA_LOCATION = 'private'
+    PRIVATE_FILE_STORAGE = 'api.upload.storage_backends.PrivateMediaStorage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+USE_MAILCHIMP = os.getenv('USE_MAILCHIMP').lower() == 'true'
+if USE_MAILCHIMP:
+    MAILCHIMP_USERNAME = os.getenv('MAILCHIMP_USERNAME')
+    MAILCHIMP_API_KEY = os.getenv('MAILCHIMP_API_KEY')
+    MAILCHIMP_LIST_ID = os.getenv('MAILCHIMP_LIST_ID')
 
 
 # Quick-start development settings - unsuitable for production
