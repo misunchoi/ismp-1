344f4d37526bb61bd898e265ad13de42bd9c635b
Jesse Woo <woojesse@gmail.com>
2020-06-27 15:09:16 -0700

    updated with aws images (#310)
    Co-authored-by: Jesse Woo <jessew@CRS-KWOO-MAC.attlocal.net>

--- frontend/src/mock-data/colleges.js
+++ frontend/src/mock-data/colleges.js
@@ -1,199 +1,127 @@
+function getCollegeImage(imageName) {
+  return 'https://ismp-us-east-1.s3.amazonaws.com/campus/' + imageName;
+}
+
 const colleges = [
   {
-    name: 'University of California, Los Angeles',
-    abbr: 'ucla',
+    name: 'Berkeley City College',
+    abbr: 'bcc',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/ucla.jpg'
+    imageUrl: getCollegeImage('berkeleycitycollege.jpg')
   },
   {
-    name: 'University of California, Berkeley',
-    abbr: 'ucb',
+    name: 'Cal State University East Bay',
+    abbr: 'csueb',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/uc-berkeley.jpg'
-  },
-  {
-    name: 'University of California, San Diego',
-    abbr: 'ucsd',
-    active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/ucsd.jpg'
-  },
-  {
-    name: 'University of North Carolina',
-    abbr: 'unc',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/unc.jpg'
-  },
-  {
-    name: 'American University',
-    location: 'Washington DC',
-    abbr: 'american',
-    active: false,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/american-university.jpg'
-  },
-  {
-    name: 'Cal Poly Pomona',
-    abbr: 'cpp',
-    active: false,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/cal-poly-pomona.jpg'
-  },
-  {
-    name: 'Cal Tech',
-    abbr: 'caltech',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/caltech.jpg'
-  },
-  {
-    name: 'Carnegie Mellon University',
-    abbr: 'cmu',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/cmu.jpg'
-  },
-  {
-    name: 'California State University, Sacramento',
-    abbr: 'sac',
-    active: true,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/csu-sacramento.jpg'
+    imageUrl: getCollegeImage('csueb.jpg')
   },
   {
     name: 'Diablo Valley College',
     abbr: 'dvc',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/dvc.jpg'
+    imageUrl: getCollegeImage('dvc.jpg')
   },
   {
     name: 'George Mason University',
     abbr: 'gmu',
     active: true,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/george-mason-university.jpg'
+    imageUrl: getCollegeImage('georgemason.jpg')
   },
   {
     name: 'George Washington University',
     abbr: 'gwu',
     active: true,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/george-washington-university.jpg'
-  },
-  {
-    name: 'Georgetown University',
-    abbr: 'georgetown',
-    active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/georgetown.jpg'
-  },
-  {
-    name: 'University of Maryland, College Park',
-    abbr: 'uom',
-    active: true,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/maryland-college-park.jpg'
-  },
-  {
-    name: 'North Carolina State',
-    abbr: 'ncstate',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/nc-state.jpg'
-  },
-  {
-    name: 'Northwestern University',
-    abbr: 'northwestern',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/northwestern.jpg'
+    imageUrl: getCollegeImage('georgewashington.jpg')
   },
   {
     name: 'Orange Coast College',
     abbr: 'occ',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/occ.jpg'
-  },
-  {
-    name: 'Princeton University',
-    abbr: 'princeton',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/princeton.jpg'
+    imageUrl: getCollegeImage('occ.jpg')
   },
   {
     name: 'Rutgers University',
     abbr: 'rutgers',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/rutgers.jpg'
-  },
-  {
-    name: 'San Francisco State University',
-    abbr: 'sfsu',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/sf-state.jpg'
-  },
-  {
-    name: 'University of Pittsburgh',
-    abbr: 'pitt',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/u-pitt.jpg'
-  },
-  {
-    name: 'University of Washington',
-    abbr: 'uw',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/u-washington.jpg'
+    imageUrl: getCollegeImage('rutgers.jpg')
+  },
+  {
+    name: 'California State University, Sacramento',
+    abbr: 'sac',
+    active: true,
+    imageUrl: getCollegeImage('sacstate.jpg')
+  },
+  {
+    name: 'University of California, Berkeley',
+    abbr: 'ucb',
+    active: true,
+    imageUrl: getCollegeImage('ucb.jpg')
   },
   {
     name: 'University of California, Davis',
     abbr: 'ucd',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/uc-davis.jpg'
+    imageUrl: getCollegeImage('ucdavis.jpg')
   },
   {
     name: 'University of California, Irvine',
     abbr: 'uci',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/uc-irvine.jpg'
+    imageUrl: getCollegeImage('uci.jpg')
   },
   {
-    name: 'University of California, Riverside',
-    abbr: 'ucr',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/uc-riverside.jpg'
+    name: 'University of California, Los Angeles',
+    abbr: 'ucla',
+    active: true,
+    imageUrl: getCollegeImage('ucla.jpg')
   },
   {
-    name: 'University of California, Santa Barbara',
-    abbr: 'ucsb',
-    active: false,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/uc-santa-barbara.jpg'
+    name: 'University of California, San Diego',
+    abbr: 'ucsd',
+    active: true,
+    imageUrl: getCollegeImage('ucsd.jpg')
   },
   {
-    name: 'University of California, Santa Cruz',
-    abbr: 'ucsc',
-    active: false,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/uc-santa-cruz.jpg'
+    name: 'University of North Carolina',
+    abbr: 'unc',
+    active: true,
+    imageUrl: getCollegeImage('unc.jpg')
   },
   {
-    name: 'University of Chicago',
-    abbr: 'uchicago',
-    active: false,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/university-of-chicago.jpg'
+    name: 'University of Maryland, College Park',
+    abbr: 'uom',
+    active: true,
+    imageUrl: getCollegeImage('umd.jpg')
   },
   {
     name: 'University of Minnesota',
     abbr: 'umn',
     active: true,
-    imageUrl:
-      'http://jessewoo.github.io/images/ismp/colleges/university-of-minnesota.jpg'
+    imageUrl: getCollegeImage('umn.jpg')
+  },
+  {
+    name: 'University of North Carolina',
+    abbr: 'unc',
+    active: true,
+    imageUrl: getCollegeImage('unc.jpg')
   },
   {
     name: 'University of Southern California',
     abbr: 'usc',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/usc.jpg'
+    imageUrl: getCollegeImage('usc.jpg')
   },
   {
     name: 'The University of Texas at Austin',
     abbr: 'uta',
     active: true,
-    imageUrl: 'http://jessewoo.github.io/images/ismp/colleges/ut-austin.jpg'
+    imageUrl: getCollegeImage('utaustin.jpg')
+  },
+  {
+    name: 'University of Washington',
+    abbr: 'uw',
+    active: true,
+    imageUrl: getCollegeImage('uw.jpg')
   }
 ];
 
