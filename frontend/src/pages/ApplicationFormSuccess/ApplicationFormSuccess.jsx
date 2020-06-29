8d8338105ef0ab638b85d9c0b59e3168a810d36f
Krikor Ailanjian <kailanjian@gmail.com>
2020-06-28 22:32:07 -0700

    Make apply success better (#323)
    * make this look nicer
    
    * add i18n
    
    * make mobile friendly
    
    stacks icon and text rather than squishing them together

--- frontend/src/pages/ApplicationFormSuccess/ApplicationFormSuccess.jsx
+++ frontend/src/pages/ApplicationFormSuccess/ApplicationFormSuccess.jsx
@@ -1,28 +1,48 @@
-import Section from '../../layout/Section';
-import React from 'react';
-import { Button, Container, Icon } from 'semantic-ui-react';
+import { Button, Container, Grid, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import React from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+
+import Section from 'layout/Section';
+import PageContainer from 'layout/PageContainer/PageContainer';
+import theme from 'styles/theme';
+
+const Heading = styled.h1`
+  font-size: ${theme.fontSizes.lg};
+`
+
+const SuccessText = styled.p`
+  font-size: ${theme.fontSizes.md};
+`
 
 const ApplicationFormSuccess = () => {
+  const { t } = useTranslation('application-form');
+
   return (
-    <Container>
+    <PageContainer>
       <Section>
-        <Icon name="check circle" size="massive" color="green" />
-        <h3>Your application has been received!</h3>
-        <p>
-          You will receive a confirmation email shortly with more details and
-          next steps. Look forward to meeting you!
-        </p>
-        <Button.Group>
-          <Button as={Link} to="/program">
-            See Programs
-          </Button>
-          <Button as={Link} to="/blog">
-            View Blog
-          </Button>
-        </Button.Group>
+        <Container text>
+          <Grid stackable verticalAlign="middle"> 
+            <Grid.Row>
+              <Grid.Column width={4} centered>
+                <Icon name="check circle" size="massive" color="green" />
+              </Grid.Column>
+              <Grid.Column width={12} centered>
+                <Heading>{t('success.title')}</Heading>
+                <SuccessText>{t('success.body')}</SuccessText>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={2} centered>
+              <Grid.Column textAlign="center">
+                <Button as={Link} to="/program">{t('success.program')}</Button>
+                <Button as={Link} to="/blog">{t('success.blog')}</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </Section>
-    </Container>
+    </PageContainer>
   );
 };
 
